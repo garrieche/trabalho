95baca1d53d0ed8d5223ebab01d76d41ff15f283
Usuario <Usuario@192.168.25.3>
2015-06-17 02:17:51 -0300

    trabalhando nos usuarios

--- trabalhoRR/src/GU/GU.java
+++ trabalhoRR/src/GU/GU.java
@@ -11,13 +11,27 @@ public class GU {
 
     public GU() throws FileNotFoundException {
         Scanner file = null;
-        String tmp;
+        int user;
+        int grupo;
 
         file = new Scanner(new File("c:\\grupos.csv"));
-    }
+        user = file.nextInt();
+        grupo = file.nextInt();
+        if (getPorNome(user) != null){
+            
+        //Usuario umUsuario = new Usuario (user, grupo);
+    }}
 
     public Usuario getLogado() {
         return logado;
+    }
+    
+    private Usuario getPorNome(int nome){
+        for (int i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].getNome() == nome)
+                return usuarios[i];
+        }
+        return null;
     }
 
     private void setLogado(Usuario logado) {
@@ -25,7 +39,17 @@ public class GU {
     }
     
     public boolean podeLogar (int nome){
-        //this.logado(exist(nome));
+        Usuario user = getPorNome(nome);
+        
+        if (user != null) {
+            setLogado(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean estaNoGrupo(int nomeDono){
         return true;
     }
 
--- trabalhoRR/src/hd/teste.java
+++ trabalhoRR/src/hd/teste.java
@@ -6,18 +6,17 @@ public class teste {
 
     public static void main(String args[]) throws IOException {
         
-        BetitanderFileSystem discoC = new BetitanderFileSystem();
-        discoC.formatar();
+//        BetitanderFileSystem discoC = new BetitanderFileSystem();
+//        discoC.formatar();
 //        for (int i = 1; i < 16; i++) {
 //            String pastinha = "/"+i;
 //            System.out.println("Criando a pastinha -> " + pastinha);
 //            discoC.criaPasta(pastinha);    
 //        }
-        discoC.criaPasta("/1");
-        discoC.criaPasta("/1/2");
-        discoC.criaArquivo("/1/7", "c://testebetitander.bin");
-        discoC.mostraPasta("/1");
-        
+//        discoC.criaPasta("/1");
+//        discoC.criaPasta("/1/2");
+//        discoC.criaArquivo("/1/7", "c://testebetitander.bin");
+//        discoC.mostraPasta("/1");
         
                 
     }
